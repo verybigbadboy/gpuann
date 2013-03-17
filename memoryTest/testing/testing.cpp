9c80928124cb8234ba8a96b56efa9a6dbe4baa0c
Alexander Sinditskiy <verybigbadboy@gmail.com>
2013-03-18 03:08:56 +0400

    remove spaces

--- memoryTest/testing/testing.cpp
+++ memoryTest/testing/testing.cpp
@@ -45,10 +45,10 @@ fann *createSpecificTrainedFann(unsigned int num_hiden_layers, unsigned int num_
   const float desired_error = (const float) 0;
   const unsigned int max_epochs = 1000;
   const unsigned int epochs_between_reports = 0;
-  
+
   struct fann *ann;
   struct fann_train_data *data;
-  
+
   unsigned int p1, p2, p3, p4, p5, p6, p7, p8, p9, p10;
   p1 = p2 = p3 = p4 = p5 = p6 = p7 = p8 = p9 = p10 = num_neurons_hidden_per_layer;
   switch (num_hiden_layers)
@@ -84,28 +84,28 @@ fann *createSpecificTrainedFann(unsigned int num_hiden_layers, unsigned int num_
       p10 = num_output;
       break;
   }
-  
+
   ann = fann_create_standard(num_layers, num_input, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10);
-  
+
   data = fann_read_train_from_file("xor.data");
-  
+
   fann_set_activation_steepness_hidden(ann, 1);
   fann_set_activation_steepness_output(ann, 1);
-  
+
   fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
   fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
-  
+
   fann_set_train_stop_function(ann, FANN_STOPFUNC_BIT);
   fann_set_bit_fail_limit(ann, 0.01f);
-  
+
   fann_set_training_algorithm(ann, FANN_TRAIN_RPROP);
-  
+
   fann_init_weights(ann, data);
-  
+
   fann_train_on_data(ann, data, max_epochs, epochs_between_reports, desired_error);
-  
+
   fann_destroy_train(data);
-  
+
   return ann;
 }
 
@@ -115,36 +115,36 @@ bool runTest(struct fann *ann, fann_type * input, const char * testName, bool fu
     printf("Test %s \n", testName);
   fann_type *calc_out;
   fann_type calc_out_gpu, calc_out_cpu;
-  
+
   calc_out = gpuann_fann_run(ann, input);
   calc_out_gpu = calc_out[0];
-  
+
   if(!fullreport)
     printf("GPU xor test (%f,%f) -> %f\n", input[0], input[1], calc_out[0]);
-  
+
   //printann(ann);
-    
-    calc_out = fann_run(ann, input);
-    calc_out_cpu = calc_out[0];
-    
-    if(!fullreport)
-      printf("CPU xor test (%f,%f) -> %f\n", input[0], input[1], calc_out[0]);
-    
-    //printann(ann);
-    
-    bool success = (calc_out_cpu - calc_out_gpu) * (calc_out_cpu - calc_out_gpu) < 0.001;
-    if(!fullreport)
-      if(success)
-        printf("Passed\n");
-      else
-        printf("Failed!!!\n");
-      return success;
+
+  calc_out = fann_run(ann, input);
+  calc_out_cpu = calc_out[0];
+
+  if(!fullreport)
+    printf("CPU xor test (%f,%f) -> %f\n", input[0], input[1], calc_out[0]);
+
+  //printann(ann);
+
+  bool success = (calc_out_cpu - calc_out_gpu) * (calc_out_cpu - calc_out_gpu) < 0.001;
+  if(!fullreport)
+    if(success)
+      printf("Passed\n");
+    else
+      printf("Failed!!!\n");
+    return success;
 }
 
 void runTests(struct fann *ann, bool fullreport)
 {
   bool success = true;
-  
+
   fann_type input[2];
   input[0] = -1;
   input[1] = -1;
@@ -158,7 +158,7 @@ void runTests(struct fann *ann, bool fullreport)
   input[0] = 1;
   input[1] = 1;
   success &= runTest(ann, input, "11", fullreport);
-  
+
   if(success)
     printf("runTests Passed\n");
   else
@@ -172,7 +172,7 @@ void fulltest()
     for(int j = 30; j < 512; j *=2)
     {
       fann *ann = createSpecificTrainedFann(i, j);
-      runTests(ann, true);
+      runTests(ann);
       fann_destroy(ann);
     }
   }
