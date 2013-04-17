5774f29e09851ffd481692b1babc3b734c0cd351
Alexander Sinditskiy <verybigbadboy@gmail.com>
2013-04-18 00:27:53 +0400

    new memcpy

--- gpuann/kernels/d2dMemcpy/simple.h
+++ gpuann/kernels/d2dMemcpy/simple.h
@@ -10,17 +10,20 @@ template <unsigned int blockSize>
 __global__ void gpuann_d2dMemcpy_gpu_kernel(fann_type *d_dst, fann_type *d_src, unsigned int size)
 {
   unsigned int tid   = threadIdx.x;
-  unsigned int index = blockIdx.x * blockSize + tid;
+  unsigned int index = tid;
 
-  if(index < size)
+  while(index < size)
+  {
     d_dst[index] = d_src[index];
+    index += blockSize;
+  }
 }
 
 void gpuann_d2dMemcpy(fann_type *d_dst, fann_type *d_src,  unsigned int size)
 {
   const unsigned int threadCount = 256;
   dim3 dimBlock(threadCount, 1, 1);
-  dim3 dimGrid(size / threadCount + 1, 1, 1);
+  dim3 dimGrid(1, 1, 1);
   
   gpuann_d2dMemcpy_gpu_kernel<threadCount> <<<dimGrid, dimBlock>>> (d_dst, d_src, size);
 }
