dddee953e5f6c3e375673a0292a851e28c6ca4f4
Alexander Sinditskiy <verybigbadboy@gmail.com>
2013-04-18 00:32:27 +0400

    Revert "new memcpy"
    This reverts commit 5774f29e09851ffd481692b1babc3b734c0cd351.

--- gpuann/kernels/d2dMemcpy/simple.h
+++ gpuann/kernels/d2dMemcpy/simple.h
@@ -10,20 +10,17 @@ template <unsigned int blockSize>
 __global__ void gpuann_d2dMemcpy_gpu_kernel(fann_type *d_dst, fann_type *d_src, unsigned int size)
 {
   unsigned int tid   = threadIdx.x;
-  unsigned int index = tid;
+  unsigned int index = blockIdx.x * blockSize + tid;
 
-  while(index < size)
-  {
+  if(index < size)
     d_dst[index] = d_src[index];
-    index += blockSize;
-  }
 }
 
 void gpuann_d2dMemcpy(fann_type *d_dst, fann_type *d_src,  unsigned int size)
 {
   const unsigned int threadCount = 256;
   dim3 dimBlock(threadCount, 1, 1);
-  dim3 dimGrid(1, 1, 1);
+  dim3 dimGrid(size / threadCount + 1, 1, 1);
   
   gpuann_d2dMemcpy_gpu_kernel<threadCount> <<<dimGrid, dimBlock>>> (d_dst, d_src, size);
 }
