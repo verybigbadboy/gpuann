6bf838ea3a864589cb021c9426ff09cb6691df31
Alexander Sinditskiy <verybigbadboy@gmail.com>
2013-04-17 12:06:15 +0400

    optimization

--- gpuann/kernels/d2dMemcpy/simple.h
+++ gpuann/kernels/d2dMemcpy/simple.h
@@ -13,14 +13,18 @@ __global__ void gpuann_d2dMemcpy_gpu_kernel(fann_type *d_dst, fann_type *d_src, 
   unsigned int index = blockIdx.x * blockSize + tid;
 
   if(index < size)
+  {
     d_dst[index] = d_src[index];
+    if(index + blockSize /2 < size)
+      d_dst[index + blockSize/2] = d_src[index + blockSize/2];
+  }
 }
 
 void gpuann_d2dMemcpy(fann_type *d_dst, fann_type *d_src,  unsigned int size)
 {
   const unsigned int threadCount = 256;
-  dim3 dimBlock(threadCount, 1, 1);
-  dim3 dimGrid(size / threadCount + 1, 1, 1);
+  dim3 dimBlock(threadCount / 2, 1, 1);
+  dim3 dimGrid(size / threadCount/2  + 1, 1, 1);
   
   gpuann_d2dMemcpy_gpu_kernel<threadCount> <<<dimGrid, dimBlock>>> (d_dst, d_src, size);
 }
