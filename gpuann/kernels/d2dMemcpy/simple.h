89c16370b55a3f268bac4eb8155c043c85ec6e39
Alexander Sinditskiy <verybigbadboy@gmail.com>
2013-04-18 22:49:39 +0400

    fast memCpy

--- gpuann/kernels/d2dMemcpy/simple.h
+++ gpuann/kernels/d2dMemcpy/simple.h
@@ -10,17 +10,21 @@ template <unsigned int blockSize>
 __global__ void gpuann_d2dMemcpy_gpu_kernel(fann_type *d_dst, fann_type *d_src, unsigned int size)
 {
   unsigned int tid   = threadIdx.x;
-  unsigned int index = blockIdx.x * blockSize + tid;
+  unsigned int index = blockIdx.x * blockSize * 2 + tid;
 
   if(index < size)
+  {
     d_dst[index] = d_src[index];
+    if(index + blockSize < size)
+      d_dst[index + blockSize] = d_src[index + blockSize];
+  }
 }
 
 void gpuann_d2dMemcpy(fann_type *d_dst, fann_type *d_src,  unsigned int size)
 {
   const unsigned int threadCount = 256;
   dim3 dimBlock(threadCount, 1, 1);
-  dim3 dimGrid(size / threadCount + 1, 1, 1);
+  dim3 dimGrid(size / threadCount / 2 + 1, 1, 1);
 
   gpuann_d2dMemcpy_gpu_kernel<threadCount> <<<dimGrid, dimBlock>>> (d_dst, d_src, size);
 }
@@ -42,13 +46,42 @@ __global__ void gpuann_d2dMemcpy_multi_gpu_kernel(fann_type *d_dst, fann_type *d
   }
 }
 
+template <unsigned int blockSize>
+__global__ void gpuann_d2dMemcpy_multi_same_values_gpu_kernel(fann_type *d_dst, fann_type *d_src, unsigned int size, unsigned int dstOffset, unsigned int instanceCount)
+{
+  unsigned int tid               = threadIdx.x;
+  unsigned int index             = blockIdx.x * blockSize * 2 + tid;
+
+  if(index < size)
+  {
+    for(unsigned int instance = 0; instance < instanceCount; ++instance)
+    {
+      unsigned int destinationOffset = dstOffset * instance;
+      d_dst[index + destinationOffset] = d_src[index];
+      if(index + blockSize < size)
+        d_dst[index + destinationOffset + blockSize] = d_src[index + blockSize];
+    }
+  }
+}
+
 void gpuann_d2dMemcpyMulti(fann_type *d_dst, fann_type *d_src,  unsigned int size, unsigned int instanceCount, unsigned int srcOffset, unsigned int dstOffset)
 {
-  const unsigned int threadCount = 256;
-  dim3 dimBlock(threadCount, 1, 1);
-  dim3 dimGrid(size / threadCount / 2 + 1, instanceCount, 1);
+  if(srcOffset == 0)
+  {
+    const unsigned int threadCount = 256;
+    dim3 dimBlock(threadCount, 1, 1);
+    dim3 dimGrid(size / threadCount / 2 + 1, 1, 1);
 
-  gpuann_d2dMemcpy_multi_gpu_kernel<threadCount> <<<dimGrid, dimBlock>>> (d_dst, d_src, size, srcOffset, dstOffset);
+    gpuann_d2dMemcpy_multi_same_values_gpu_kernel<threadCount> <<<dimGrid, dimBlock>>> (d_dst, d_src, size, dstOffset, instanceCount);
+  }
+  else
+  {
+    const unsigned int threadCount = 256;
+    dim3 dimBlock(threadCount, 1, 1);
+    dim3 dimGrid(size / threadCount / 2 + 1, instanceCount, 1);
+
+    gpuann_d2dMemcpy_multi_gpu_kernel<threadCount> <<<dimGrid, dimBlock>>> (d_dst, d_src, size, srcOffset, dstOffset);
+  }
 }
 
 
