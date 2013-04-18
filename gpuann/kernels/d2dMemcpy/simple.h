2f1a473399b143e62d635cfac7337f0a27feba23
Alexander Sinditskiy <verybigbadboy@gmail.com>
2013-04-18 22:01:19 +0400

    memcpy copy 2 values per thread

--- gpuann/kernels/d2dMemcpy/simple.h
+++ gpuann/kernels/d2dMemcpy/simple.h
@@ -29,21 +29,26 @@ template <unsigned int blockSize>
 __global__ void gpuann_d2dMemcpy_multi_gpu_kernel(fann_type *d_dst, fann_type *d_src, unsigned int size, unsigned int srcOffset, unsigned int dstOffset)
 {
   unsigned int tid               = threadIdx.x;
-  unsigned int index             = blockIdx.x * blockSize + tid;
+  unsigned int index             = blockIdx.x * blockSize * 2 + tid;
   unsigned int instance          = blockIdx.y;
   unsigned int destinationOffset = dstOffset * instance;
   unsigned int sourceOffset      = srcOffset * instance;
 
   if(index < size)
+  {
     d_dst[index + destinationOffset] = d_src[index + sourceOffset];
+    if(index + blockSize < size)
+      d_dst[index + destinationOffset + blockSize] = d_src[index + sourceOffset + blockSize];
+  }
 }
 
 void gpuann_d2dMemcpyMulti(fann_type *d_dst, fann_type *d_src,  unsigned int size, unsigned int instanceCount, unsigned int srcOffset, unsigned int dstOffset)
 {
   const unsigned int threadCount = 256;
   dim3 dimBlock(threadCount, 1, 1);
-  dim3 dimGrid(size / threadCount + 1, instanceCount, 1);
+  dim3 dimGrid(size / threadCount / 2 + 1, instanceCount, 1);
 
   gpuann_d2dMemcpy_multi_gpu_kernel<threadCount> <<<dimGrid, dimBlock>>> (d_dst, d_src, size, srcOffset, dstOffset);
 }
+
 
