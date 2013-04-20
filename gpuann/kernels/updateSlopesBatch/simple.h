d2890a217f1863d37ff6ba34c1da8f873173225e
Alexander Sinditskiy <verybigbadboy@gmail.com>
2013-04-21 02:55:04 +0400

    removed comment

--- gpuann/kernels/updateSlopesBatch/simple.h
+++ gpuann/kernels/updateSlopesBatch/simple.h
@@ -145,37 +145,3 @@ void gpuann_fann_update_slopes_batch_implementation(gpuann &data, fann_layer *la
     }
   }
 }
-
-/* ORIGINAL
-void fann_update_slopes_batch(struct fann *ann, struct fann_layer *layer_begin, struct fann_layer *layer_end)
-{
-  struct fann_neuron *neuron_it, *last_neuron, *prev_neurons;
-  fann_type tmp_error;
-  unsigned int i, num_connections;
-
-  struct fann_neuron *first_neuron = ann->first_layer->first_neuron;
-  fann_type *error_begin = ann->train_errors;
-  fann_type *slope_begin, *neuron_slope;
-
-  slope_begin = ann->train_slopes;
-
-  prev_neurons = first_neuron;
-
-  for(; layer_begin <= layer_end; layer_begin++)
-  {
-    last_neuron = layer_begin->last_neuron;
-    prev_neurons = (layer_begin - 1)->first_neuron;
-
-    for(neuron_it = layer_begin->first_neuron; neuron_it != last_neuron; neuron_it++)
-    {
-      tmp_error = error_begin[neuron_it - first_neuron];
-      neuron_slope = slope_begin + neuron_it->first_con;
-      num_connections = neuron_it->last_con - neuron_it->first_con;
-      for(i = 0; i != num_connections; i++)
-      {
-        neuron_slope[i] += tmp_error * prev_neurons[i].value;
-      }
-    }
-  }
-}
-*/
