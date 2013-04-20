d6cc93f80a61c15e70e2aa5db3b6c2abcc71d91e
Alexander Sinditskiy <verybigbadboy@gmail.com>
2013-04-21 02:03:58 +0400

    test backpropagation improvement

--- gpuann/kernels/backpropagateMSE/simple.h
+++ gpuann/kernels/backpropagateMSE/simple.h
@@ -121,6 +121,41 @@ __global__ void fann_backpropagate_MSE_gpu_kernel(const unsigned int prevNeurons
     prevTrainErrors[prevLayerNeuronInstanced] = sum[0] * gpuann_fann_activation_derived<prevActivationFunction>(prevSteepness, prevValue[prevLayerNeuronInstanced], prevSum[prevLayerNeuronInstanced]);
 }
 
+template <unsigned int blockSize, unsigned int prevActivationFunction>
+__global__ void fann_backpropagate_MSE_parallel_gpu_kernel(const unsigned int prevNeuronsCount,
+                                                  const unsigned int neuronsCount,
+                                                  fann_type *weights,
+                                                  fann_type *trainErrors,
+                                                  fann_type *prevTrainErrors,
+                                                  fann_type *prevValue,
+                                                  fann_type *prevSum,
+                                                  fann_type prevSteepness,
+                                                  const unsigned int totalNeuronsCount,
+                                                  const unsigned int totalWeightsCount)
+{
+  const unsigned int tid                      = threadIdx.x;
+  const unsigned int instance                 = blockIdx.y;
+  const unsigned int weightPerNeuronCount     = prevNeuronsCount;
+  const unsigned int prevLayerNeuron          = tid + blockSize * blockIdx.x;
+  const unsigned int prevLayerNeuronInstanced = prevLayerNeuron + instance * totalNeuronsCount;
+
+  fann_type mySum = 0;
+  unsigned int neuronIndex = 0;
+  if(prevLayerNeuron < prevNeuronsCount)
+  {
+    while (neuronIndex < neuronsCount)
+    {
+      const unsigned int neuronIndexInstanced = neuronIndex + instance * totalNeuronsCount;
+      const unsigned int weightBeginIndex     = neuronIndex * weightPerNeuronCount + instance * totalWeightsCount;
+
+      mySum += trainErrors[neuronIndexInstanced] * weights[weightBeginIndex + prevLayerNeuron];
+      neuronIndex++;
+    }
+
+    prevTrainErrors[prevLayerNeuronInstanced] = mySum * gpuann_fann_activation_derived<prevActivationFunction>(prevSteepness, prevValue[prevLayerNeuronInstanced], prevSum[prevLayerNeuronInstanced]);
+  }
+}
+
 template <unsigned int prevActivationFunction>
 __global__ void fann_backpropagate_MSE_multineuron_gpu_kernel(unsigned int prevNeuronsCount,
                                                                      unsigned int neuronsCount,
@@ -200,7 +235,7 @@ fann_backpropagate_MSE_multineuron_gpu_kernel<X> <<<dimGrid, dimBlock>>> (prevNe
 break;
 
 #define fann_backpropagate_MSE_gpu_kernel_case(X)   case X: \
-fann_backpropagate_MSE_gpu_kernel<blockSize, X> <<<dimGrid, dimBlock>>> (prevNeuronsCount, neuronsCount, weights, trainErrors, prevTrainErrors, prevValue, prevSum, prevSteepness, totalNeuronsCount, totalWeightsCount); \
+fann_backpropagate_MSE_parallel_gpu_kernel<blockSize, X> <<<dimGrid, dimBlock>>> (prevNeuronsCount, neuronsCount, weights, trainErrors, prevTrainErrors, prevValue, prevSum, prevSteepness, totalNeuronsCount, totalWeightsCount); \
 break;
 
 template <unsigned int blockSize>
@@ -208,7 +243,7 @@ void fann_backpropagate_MSE_gpu_kernel_activationFunction(unsigned int instanceC
 , unsigned int totalNeuronsCount, unsigned int totalWeightsCount)
 {
   dim3 dimBlock(blockSize, 1, 1);
-  dim3 dimGrid(prevNeuronsCount, instanceCount, 1);
+  dim3 dimGrid(neuronsCount / blockSize + 1, instanceCount, 1);
 
   switch(prevActivationFunction)
   {
@@ -270,7 +305,7 @@ void fann_backpropagate_MSE_gpu_kernel_blockSize(unsigned int instanceCount, uns
   }
   else
   {
-    unsigned int threadsCount = pow2roundup(neuronsCount);
+    unsigned int threadsCount = pow2roundup(prevNeuronsCount);
 
     if(threadsCount < 32)
       threadsCount = 32;
@@ -288,6 +323,7 @@ void fann_backpropagate_MSE_gpu_kernel_blockSize(unsigned int instanceCount, uns
     }
   }
 }
+
 
 void gpuann_fann_backpropagate_MSE_implementation_gpu(gpuann &data)
 {
