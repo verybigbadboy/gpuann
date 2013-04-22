6e228a3c89cc97808f2c84fee03dc9fbbb389a5f
Alexander Sinditskiy <verybigbadboy@gmail.com>
2013-04-22 20:24:28 +0400

    update slopes implementation changes

---
+++ gpuann/kernels/updateSlopesBatch/bigNeuron.h
@@ -0,0 +1,67 @@
+
+//neuronSlopes actually points to prev layer values
+template <unsigned int blockSize>
+__global__ void gpuann_fann_update_slopes_batch_big_neurons_gpu_kernel(unsigned int prevNeuronsCount,
+                                                           unsigned int neuronsCount,
+                                                           fann_type *trainErrors,
+                                                           fann_type *neuronSlopes,
+                                                           fann_type *prevValue,
+                                                           unsigned int totalNeuronsCount,
+                                                           unsigned int totalWeightsCount)
+{
+  unsigned int tid                  = threadIdx.x;
+  unsigned int instance             = blockIdx.y;
+  unsigned int neuronIndex          = 0;
+  unsigned int prevLayerNeuronIndex = tid + blockSize * blockIdx.x;
+
+
+  unsigned int prevLayerNeuronIndexInstanced = prevLayerNeuronIndex + instance * totalNeuronsCount;
+  fann_type prev_value = prevValue[prevLayerNeuronIndexInstanced];
+
+  unsigned int slopesIndexInstanced;
+  if(prevLayerNeuronIndex < prevNeuronsCount)
+  {
+    while(neuronIndex < neuronsCount) //TODO BIAS
+    {
+      unsigned int neuronIndexInstanced = neuronIndex + instance * totalNeuronsCount;
+      fann_type error = trainErrors[neuronIndexInstanced];
+      slopesIndexInstanced          = prevLayerNeuronIndex + prevNeuronsCount * neuronIndex + instance * totalWeightsCount;
+      neuronSlopes[slopesIndexInstanced] += error * prev_value;
+      ++neuronIndex;
+    }
+  }
+}
+
+void gpuann_fann_update_slopes_batch_big_neurons_implementation(unsigned int prevNeuronsCount,
+                                                                unsigned int neuronsCount,
+                                                                fann_type   *trainErrors,
+                                                                fann_type   *neuronSlopes,
+                                                                fann_type   *prevValue,
+                                                                unsigned int totalNeuronsCount,
+                                                                unsigned int totalWeightsCount,
+                                                                unsigned int instanceCount)
+{
+#define gpuann_fann_update_slopes_batch_big_neurons_gpu_kernelCase(X)   case X: \
+gpuann_fann_update_slopes_batch_big_neurons_gpu_kernel<X> <<<dimGrid, dimBlock>>> (prevNeuronsCount, neuronsCount, trainErrors, neuronSlopes, prevValue, totalNeuronsCount, totalWeightsCount); \
+break;
+
+  unsigned int threadCount = pow2roundup(prevNeuronsCount) / 2;
+  if(threadCount < 32)
+    threadCount = 32;
+
+  //if(threadCount > 256)
+  threadCount = 256;
+
+  dim3 dimBlock(threadCount, 1, 1);
+  dim3 dimGrid(prevNeuronsCount / threadCount + 1, instanceCount, 1); // TODO create bias if
+  
+  neuronsCount--;
+
+  switch (threadCount)
+  {
+    gpuann_fann_update_slopes_batch_big_neurons_gpu_kernelCase(32);
+    gpuann_fann_update_slopes_batch_big_neurons_gpu_kernelCase(64);
+    gpuann_fann_update_slopes_batch_big_neurons_gpu_kernelCase(128);
+    gpuann_fann_update_slopes_batch_big_neurons_gpu_kernelCase(256);
+  }
+}
---
+++ gpuann/kernels/updateSlopesBatch/main.h
@@ -0,0 +1,68 @@
+#include <kernels/updateSlopesBatch/updateSlopesBatch.h>
+#include <fann.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <common/math.h>
+#include <string>
+
+#include <kernels/updateSlopesBatch/simple.h>
+#include <kernels/updateSlopesBatch/multineuron.h>
+#include <kernels/updateSlopesBatch/bigNeuron.h>
+
+
+void gpuann_fann_update_slopes_batch_implementation(gpuann &data, fann_layer *layerBegin, fann_layer *layerEnd)
+{
+  const fann *ann = data._fann;
+  unsigned int instanceCount = data._instanceCount;
+  fann_neuron *firstNeuron = ann->first_layer->first_neuron;
+  fann_layer *layerIt;
+
+  for(layerIt = layerBegin; layerIt <= layerEnd; ++layerIt)
+  {
+    unsigned int layerSize = layerIt->last_neuron - layerIt->first_neuron;
+    unsigned int layerNeuronShift = layerIt->first_neuron - firstNeuron;
+    unsigned int layerWeightShift = layerIt->first_neuron->first_con;
+
+    fann_layer  *prevLayer = layerIt - 1;
+    fann_neuron *prevLayerFirstNeuron = prevLayer->first_neuron;
+    unsigned int prevLayerNeuronShift = prevLayerFirstNeuron - firstNeuron;
+    unsigned int prevLayerSize = prevLayer->last_neuron - prevLayerFirstNeuron;
+
+    if(prevLayerSize < 32)
+    {
+      gpuann_fann_update_slopes_batch_multineuron_implementation(prevLayerSize,
+                                                                 layerSize,
+                                                                 &(data.d_trainErrorsArray[layerNeuronShift]),
+                                                                 &(data.d_trainSlopes[layerWeightShift]),
+                                                                 &(data.d_valuesArray[prevLayerNeuronShift]),
+                                                                 data._neuronsCountPerInstance,
+                                                                 data._weightsCountPerInstance,
+                                                                 instanceCount);
+    }
+    else
+    {
+      if(prevLayerSize > 256)
+      {
+        gpuann_fann_update_slopes_batch_big_neurons_implementation(prevLayerSize,
+                                                                   layerSize,
+                                                                   &(data.d_trainErrorsArray[layerNeuronShift]),
+                                                                   &(data.d_trainSlopes[layerWeightShift]),
+                                                                   &(data.d_valuesArray[prevLayerNeuronShift]),
+                                                                   data._neuronsCountPerInstance,
+                                                                   data._weightsCountPerInstance,
+                                                                   instanceCount);
+      }
+      else
+      {
+        gpuann_fann_update_slopes_batch_simple_implementation(prevLayerSize,
+                                                              layerSize,
+                                                              &(data.d_trainErrorsArray[layerNeuronShift]),
+                                                              &(data.d_trainSlopes[layerWeightShift]),
+                                                              &(data.d_valuesArray[prevLayerNeuronShift]),
+                                                              data._neuronsCountPerInstance,
+                                                              data._weightsCountPerInstance,
+                                                              instanceCount);
+      }
+    }
+  }
+}
